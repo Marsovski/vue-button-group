f83de686a32d6007ecb426af9bd99406a0c0be34
Milos Markovic <milos.markovic@outlook.com>
2017-03-23 22:16:26 +0100

    webpack update and demo

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,34 @@
         </app-group-button>
         <hr>
         <pre>{{arraySelect}}</pre>
+
+        <h1>Demo 1</h1>
+        <app-group-button
+                :typeArray="false"
+                :states="demoOneList"
+                :state="demoOneSelect"
+                @selectButton="demoOneSelect = $event"
+        >
+        </app-group-button>
+        <hr>
+        <pre>{{demoOneSelect}}</pre>
+
+        <h1>Demo 2</h1>
+        <app-group-button
+                :states="demoTwoList"
+                :state="demoTwoSelect"
+                @selectButton="demoTwoSelect = $event"
+        >
+        </app-group-button>
+        <hr>
+        <pre>{{demoTwoSelect}}</pre>
     </div>
 </template>
 
 <script>
     import GroupButton from './components/GroupButton.vue';
     export default {
+        name: 'app',
         data: function () {
             return {
                 objectSelect: [{id: 3}, {id: 5}],
@@ -48,7 +70,33 @@
                     'Fri',
                     'Sat',
                     'Sun'
-                ]
+                ],
+                demoOneSelect: [],
+                demoOneList: [
+                    {id: 1, name: '1'},
+                    {id: 2, name: '2'},
+                    {id: 3, name: '3'},
+                    {id: 4, name: '4'},
+                    {id: 5, name: '5'},
+                    {id: 6, name: '6'},
+                    {id: 7, name: '7'},
+                    {id: 8, name: '8'},
+                    {id: 9, name: '9'},
+                    {id: 10, name: '10'}
+                ],
+                demoTwoSelect: [],
+                demoTwoList: [
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7,
+                    8,
+                    9,
+                    10
+                ],
             }
         },
         components: {
@@ -58,4 +106,7 @@
 </script>
 
 <style>
+    pre {
+        height: 200px;
+    }
 </style>
